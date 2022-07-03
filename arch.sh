580e6834603df1a193cc193d603b6911a2743dff
The Glitchh <thefallnn@pm.me>
2022-07-03 13:57:04 +0530

    readme: typo at package definition

--- arch.sh
+++ arch.sh
@@ -3,7 +3,7 @@ sudo pacman -Syu
 sudo pacman -S htop bashtop neofetch vlc ffmpeg rust  zsh packagekit-qt5 latte-dock 
 echo -e "installing i3 and required components"
 sleep 2s
-sudo pacman -S i3-gaps gnome-disk-utility i3lock i3block gnome-screenshot rofi alacritty nautilus gedit simplescreenrecorder
+sudo pacman -S i3-gaps gnome-disk-utility i3lock i3blocks gnome-screenshot rofi alacritty nautilus gedit simplescreenrecorder
 echo -e "getting some requried package from AUR"
 yay -S polybar cava picom-jonaburg-git
 echo -e "setting up zsh"
