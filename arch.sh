0816a972752dcc0c026d563e479b6e7f5cb54b51
sourav2k <souravgope765@gmail.com>
2022-11-14 12:29:28 +0530

    setup: add back more required packages

--- arch.sh
+++ arch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 sudo pacman -Syu 
-sudo pacman -S htop bashtop neofetch vlc ffmpeg rust  zsh packagekit-qt5 latte-dock 
+sudo pacman -S htop bashtop neofetch vlc ffmpeg rust  zsh packagekit-qt5 latte-dock gimp zenity
 echo -e "installing i3 and required components"
 sleep 2s
 sudo pacman -S i3-gaps gnome-disk-utility i3lock i3blocks gnome-screenshot rofi alacritty nautilus gedit simplescreenrecorder 
 echo -e "installing required fonts for arch linux"
 sudo pacman -S ttf-caladea ttf-carlito ttf-dejavu ttf-liberation noto-fonts ttf-opensans otf-overpass ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-dejavu ttf-liberation noto-fonts 
 echo -e "getting some requried package from AUR"
-yay -S polybar cava picom-jonaburg-git vim-plug spicetify-cli pipes.sh bonsai.sh-git ufetch-git 
+yay -S polybar cava picom-jonaburg-git vim-plug spicetify-cli pipes.sh bonsai.sh-git ufetch-git gtop 
 echo -e "setting up zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo -e "installing zsh jovial"
