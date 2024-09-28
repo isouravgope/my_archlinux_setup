5a4133b23210d5a055986e66f342f6ca57461866
Sourav Gope <souravgope765@gmail.com>
2024-09-28 23:35:25 +0530

    script: add nvim python package

--- arch.sh
+++ arch.sh
@@ -3,7 +3,7 @@ sudo pacman -Syu
 sudo pacman -S htop bashtop neofetch vlc ffmpeg rust  zsh packagekit-qt5 latte-dock gimp zenity
 echo -e "installing i3 and required components"
 sleep 2s
-sudo pacman -S i3-gaps gnome-disk-utility i3lock i3blocks gnome-screenshot rofi alacritty nautilus gedit simplescreenrecorder 
+sudo pacman -S i3-gaps gnome-disk-utility i3lock i3blocks gnome-screenshot rofi alacritty nautilus gedit simplescreenrecorder python-pynvim
 echo -e "installing required fonts for arch linux"
 sudo pacman -S ttf-caladea ttf-carlito ttf-dejavu ttf-liberation noto-fonts ttf-opensans otf-overpass ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-dejavu ttf-liberation noto-fonts 
 echo -e "getting some requried package from AUR"
